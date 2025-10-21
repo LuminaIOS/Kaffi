33fba7f39552853cc7c52519329ac3895eecfaec
Amparo Alcaraz <145872798+Amparoalcarazt@users.noreply.github.com>
2025-10-21 16:40:22 -0600

    Create xcode proyect
    se creo el proyecto default de xcode

---
+++ Kaffi/Kaffi/KaffiApp.swift
@@ -0,0 +1,17 @@
+//
+//  KaffiApp.swift
+//  Kaffi
+//
+//  Created by Amparo Alcaraz Tonella on 21/10/25.
+//
+
+import SwiftUI
+
+@main
+struct KaffiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
