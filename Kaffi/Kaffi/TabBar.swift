c4397615b5488412a3c059d01a63dcc2ea9f9b69
Amparo Alcaraz <145872798+Amparoalcarazt@users.noreply.github.com>
2025-10-21 17:40:29 -0600

    Create TabBar & Declare colors
    Cree la vista de tabBar que ayudara a la navegacion del Tecnico y se definieron los colores a utilizar para el proyecto como variables globales

---
+++ Kaffi/Kaffi/TabBar.swift
@@ -0,0 +1,85 @@
+//
+//  NavBar.swift
+//  Kaffi
+//
+//  Created by Amparo Alcaraz Tonella on 21/10/25.
+//
+//
+
+import SwiftUI
+
+struct TabBarView: View {
+    @State private var searchText = ""
+    
+    
+    var body: some View {
+        TabView {
+            NavigationStack {
+                TDashboardView()
+                    .navigationTitle("Dashboard")
+                    .toolbarColorScheme(.dark, for: .navigationBar)
+                    .toolbarBackground(darkColor2, for: .navigationBar)
+                    .toolbarBackground(.visible, for: .navigationBar)
+                    .navigationBarTitleDisplayMode(.inline)
+            }
+            .tabItem { Label("Dashboard", systemImage: "house.fill") }
+            
+            NavigationStack {
+                TFincasView()
+                    .navigationTitle("Fincas").toolbarColorScheme(.dark, for: .navigationBar)
+                    .toolbarBackground(darkColor2, for: .navigationBar)
+                    .toolbarBackground(.visible, for: .navigationBar)
+                    .navigationBarTitleDisplayMode(.inline)
+            }
+            .tabItem { Label("Fincas", systemImage: "map.fill") }
+            
+            NavigationStack {
+                TLotesView()
+                    .navigationTitle("Lotes").toolbarColorScheme(.dark, for: .navigationBar)
+                    .toolbarBackground(darkColor2, for: .navigationBar)
+                    .toolbarBackground(.visible, for: .navigationBar)
+                    .navigationBarTitleDisplayMode(.inline)
+            }
+            .tabItem { Label("Lotes", systemImage: "mappin") }
+            
+            // configurations
+            NavigationStack {
+                TPerfilView()
+                    .navigationTitle("Perfil").toolbarColorScheme(.dark, for: .navigationBar)
+                    .toolbarBackground(darkColor2, for: .navigationBar)
+                    .toolbarBackground(.visible, for: .navigationBar)
+                    .navigationBarTitleDisplayMode(.inline)
+            }
+            .tabItem { Label("Perfil", systemImage: "person") }
+        }
+        .tint(midColor1)
+    }
+}
+
+struct TDashboardView: View {
+    @State private var searchText = ""
+    var body: some View {
+        Text("dashboard")
+    }
+}
+struct TFincasView: View {
+    @State private var searchText = ""
+    var body: some View {
+        Text("Fincas")
+    }
+}
+struct TLotesView: View {
+    @State private var searchText = ""
+    var body: some View {
+        Text("Lotes")
+    }
+}
+struct TPerfilView: View {
+    @State private var searchText = ""
+    var body: some View {
+        Text("Perfil")
+    }
+}
+#Preview {
+    TabBarView()
+}
